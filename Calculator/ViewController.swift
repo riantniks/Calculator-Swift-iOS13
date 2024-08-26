c46d427a23c8fd140d83e88ed1df29e6bafbe446
Rahul <r.ashokayan@gmail.com>
2024-08-26 19:10:48 +0530

    Before access level challenge

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,11 +12,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
+    private var isFinishedTypingNumber: Bool = true     // scope limited to this class ViewController
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
+
+        isFinishedTypingNumber = true
     
     }
 
@@ -24,7 +27,17 @@ class ViewController: UIViewController {
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+
+        if let numValue = sender.currentTitle {
+
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+            
+        }
     }
 
 }
