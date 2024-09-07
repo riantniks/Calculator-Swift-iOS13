53cc3c900c392cb5bbcb58282c1af0984abd3e0f
Rahul <r.ashokayan@gmail.com>
2024-09-07 17:43:59 +0530

    Final Calculator project

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -10,25 +10,55 @@ import Foundation
 
 class CalculatorLogic {
 
-    var number: Double
+    private var number: Double?
     
-    init(number: Double) {
-        self.number = number        //number = number or number = n when init(n: Double)
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+
+    //'-' means we are setting external parameter name to be nothing and the internal parameter name to be 'number'
+    mutating func setNumber(_ number: Double) {
+        self.number = number
     }
     
-    
-    func calculate(symbol: String) -> Double? {
+    mutating func calculate(symbol: String) -> Double? {
         
-        if symbol == "+/-" {
-            return number * -1
-        } else if symbol == "%" {
-            return number * 0.01
-        } else if symbol == "AC" {
-            return 0
+        if let n = number {
+            switch symbol {
+                case "+/-":
+                    return n * -1
+                case "%":
+                    return n * 0.01
+                case "AC":
+                    return 0
+                case "=":
+                    return performTwoNumCalculation(n2: n)
+                default:    //symbol is either "+", "-", "×" or "÷"
+                    intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
         }
         
         return nil
-    
 
     }
+    
+    private func performTwoNumCalculation(n2: Double) -> Double? {
+        
+        if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod {
+            switch operation {
+                case "+":
+                    return n1 + n2
+                case "-":
+                    return n1 - n2
+                case "×":
+                    return n1 * n2
+                case "÷":
+                    return n1 / n2
+                default:
+                    fatalError("The operation passed in does not match any of the cases")
+            }
+        }
+        
+        return nil
+        
+    }
+    
 }
